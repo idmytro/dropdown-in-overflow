c43ef99baf4d39f7dcd10f9c6500be7ea2a11a1c
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-08-23 18:55:15 +0300

    bs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 
+import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 // eslint-disable-next-line import/no-unresolved
 import 'windi.css';
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
 
 Vue.use(ElementUI);
 
---
+++ src/views/BsVue.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-10px">
+    <div class="p-20px mx-auto w-400px border bg-green-100 overflow-hidden">
+      <b-dropdown
+      id="dropdown-1"
+      text="Dropdown Button"
+      boundary="window"
+      class="m-md-2"
+      >
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item active>Active action</b-dropdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
+    </div>
+</div>
+</template>
