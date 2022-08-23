d3913da9328bbba74b9162a4cf39caf2d90dfcf7
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-08-23 18:37:06 +0300

    done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+
 import App from './App.vue';
 import router from './router';
+// eslint-disable-next-line import/no-unresolved
+import 'windi.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="my-10px">
+    <div class="p-20px mx-auto w-400px border bg-green-100 overflow-hidden">
+      <el-dropdown>
+        <el-button type="primary">
+          Dropdown List<i class="el-icon-arrow-down el-icon--right"></i>
+        </el-button>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>Action 1</el-dropdown-item>
+          <el-dropdown-item>Action 2</el-dropdown-item>
+          <el-dropdown-item>Action 3</el-dropdown-item>
+          <el-dropdown-item>Action 4</el-dropdown-item>
+          <el-dropdown-item>Action 5</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import 'element-ui/lib/theme-chalk/index.css';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
   },
 };
 </script>
+
+<style>
+.el-dropdown {
+  vertical-align: top;
+}
+.el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+</style>
