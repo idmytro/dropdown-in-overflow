3115d05666b6d1c71aff2019dc37cc234ccb26d2
Dmytro <dmytro.cisar@gmail.com>
2022-12-26 15:02:15 +0200

    fixed

---
+++ src/views/BsVueFixed.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable max-len -->
+<template>
+  <div class="my-10px">
+    <div class="p-20px mx-auto w-400px border bg-green-100 overflow-hidden absolute top-1rem left-1rem">
+      <b-dropdown
+        id="dropdown-1"
+        text="Dropdown Button"
+        :popper-opts="{positionFixed: true}"
+        class="m-md-2"
+      >
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item active>Active action</b-dropdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
+    </div>
+  </div>
+</template>
